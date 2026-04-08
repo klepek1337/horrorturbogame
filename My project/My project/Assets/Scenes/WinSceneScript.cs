a2b29d82f37f83d64b5857047221b42fb048794c
m1shokk <michel1darg@gmail.com>
2026-04-08 12:08:45 +0200

    fix: bug with cursor

--- My project/My project/Assets/Scenes/WinSceneScript.cs
+++ My project/My project/Assets/Scenes/WinSceneScript.cs
@@ -3,18 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class WinSceneScript : MonoBehaviour
 {
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 1f;
+    }
+
     public void OnRestartButton()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         SceneManager.LoadScene("MainScene");
     }
 
     public void OnMenuButton()
     {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         SceneManager.LoadScene("Menu");
     }
 
     public void OnQuitButton()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
